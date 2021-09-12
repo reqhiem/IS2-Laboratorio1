c2f55ca6780e07d2ca5f42b309ea5cd218025794
reqhiem <jperca@unsa.edu.pe>
2021-09-11 20:53:28 -0500

    editando en la rama backend (creacion de una nueva ruta)

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Laboratorio 1' });
 });
+
+/*GET saludos page*/
+router.get('/greeting', function(req,res){
+  res.send('Hola :D')
+})
 
 module.exports = router;
